4a353274b7d4e14ad006b7f3b9c6767301ea1c19
EC2 Default User <ec2-user@ip-172-31-34-36.ap-northeast-1.compute.internal>
2024-07-11 10:15:29 +0000

    porse

--- rss_aws_whatsnew/db.py
+++ rss_aws_whatsnew/db.py
@@ -3,18 +3,20 @@ from datetime import datetime, timedelta
 import time
 import re
 
+
 class FeedEntryDB:
     """
     define DB objects
     """
+
     def __init__(self, db_name):
         self.db_name = db_name
         # self.init_db()
-        # "OpenSearch Service", "OpenSearch Service - Ingestion", "OpenSearch Service - Serverless", "OpenSearch Service - Managed Cluster" -> "OpenSearch"
-        # "Managed Service for Prometheus", Grafana, Kafka -> Prometheus, Grafana, Kafka
-        # Kinesis Analytics", "Kinesis Firehose", "Kinesis Streams" -> "Kinesis"
+        # "OpenSearch Service", "OpenSearch Service - Ingestion", "OpenSearch Service - Serverless", "OpenSearch Service - Managed Cluster" -> "OpenSearch"  # noqa: E501
+        # "Managed Service for Prometheus", Grafana, Kafka -> Prometheus, Grafana, Kafka  # noqa: E501
+        # Kinesis Analytics", "Kinesis Firehose", "Kinesis Streams" -> "Kinesis" # noqa: E501
         self.target_services = [
-            "Kinesis", "OpenSearch Service", "QuickSight", "Redshift", "Kendra",
+            "Kinesis", "OpenSearch", "QuickSight", "Redshift", "Kendra",
             "AppFlow", "DataZone", "Grafana", "Prometheus", "Kafka", "Neptune",
             "CloudSearch", "Q Business",
             "Supply Chain", "Honeycode", "Lookout for Metrics"
@@ -24,6 +26,7 @@ class FeedEntryDB:
         """
         init sqlite db and create table
         """
+
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
         c.execute(
@@ -53,12 +56,15 @@ class FeedEntryDB:
         """
         loading rss feed data and insert to sqlite table.
         """
+
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
         for entry in feed.entries:
             published_parsed = entry.get('published_parsed', None)
             if published_parsed:
-                published_parsed = datetime.fromtimestamp(time.mktime(published_parsed)).strftime('%Y-%m-%d %H:%M:%S')
+                published_parsed = datetime.fromtimestamp(
+                                    time.mktime(published_parsed)
+                                    ).strftime('%Y-%m-%d %H:%M:%S')
             else:
                 published_parsed = ''
             values = (
@@ -78,7 +84,10 @@ class FeedEntryDB:
                 entry.get('link', '')
             )
             try:
-                c.execute("INSERT OR REPLACE INTO feed_entries VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", values)
+                c.execute(
+                        "INSERT OR REPLACE INTO feed_entries VALUES "
+                        "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", values
+                )
             except sqlite3.ProgrammingError as e:
                 print(f"データ形式が変更された可能性があります: {e}")
         conn.commit()
@@ -88,49 +97,52 @@ class FeedEntryDB:
         """
         get articles last n days
         """
-        
+
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
         recent_date = datetime.now() - timedelta(days=days)
         print("recent_date:", recent_date)
-        c.execute("SELECT * FROM feed_entries WHERE published_parsed >= ?", (recent_date.strftime('%Y-%m-%d %H:%M:%S'),))
+        c.execute(
+                "SELECT * FROM feed_entries WHERE published_parsed >= ?",
+                (recent_date.strftime('%Y-%m-%d %H:%M:%S'),)
+        )
         rows = c.fetchall()
         print("rows", len(rows))
         conn.close()
         return rows
-    
+
     def delete_old_entries(self, days=30):
         """
-        delete articles before n 
+        delete articles before n
         """
+
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
-
-        # get table length before deleting
-        c.execute("SELECT COUNT(*) FROM feed_entries")
+        #  get table length before deleting
+        c.execute(
+                "SELECT COUNT(*) FROM feed_entries"
+        )
         table_size_before_delete = c.fetchone()[0]
-
         recent_date = datetime.now() - timedelta(days=days)
         print("recent_date:", recent_date)
-        c.execute("DELETE FROM feed_entries WHERE published_parsed < ?", (recent_date.strftime('%Y-%m-%d %H:%M:%S'),))
+        c.execute(
+                "DELETE FROM feed_entries WHERE published_parsed < ?",
+                (recent_date.strftime('%Y-%m-%d %H:%M:%S'),)
+        )
         conn.commit()
-
-        # get table length after deleting
+        #  get table length after deleting
         c.execute("SELECT COUNT(*) FROM feed_entries")
         table_size_after_delete = c.fetchone()[0]
-        print("delete articles num:", table_size_before_delete - table_size_after_delete)
+        print("deleting:", table_size_before_delete - table_size_after_delete)
         conn.close()
-    
+
     def contains_service_word(self, text):
         for service_words in self.target_services:
-            
-            if re.search(r'\b' + re.escape(service_words) + r'\b', text, re.IGNORECASE):
-                #re.escapeを使って正規表現のメタ文字をエスケープしています。
-                #\bを使って単語の境界を指定することで、サービス名の単語が含まれる場合のみマッチ
-                #re.IGNORECASEを指定することで、大文字小文字を区別せずに検索
+            pattern = r'\b' + re.escape(service_words) + r'\b'
+            if re.search(pattern, text, re.IGNORECASE):
+                #  re.escapeを使って正規表現のメタ文字をエスケープしています。
+                #  \bで単語の境界を指定して、サービス名の単語が含まれる場合のみマッチ
+                #  re.IGNORECASEを指定することで、大文字小文字を区別せずに検索
                 print("service true:", service_words)
                 return True
-        
         return False
-
-
