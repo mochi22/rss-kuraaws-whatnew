5a57341b20c0b6469afb989035090fbfda75ad65
EC2 Default User <ec2-user@ip-172-31-34-36.ap-northeast-1.compute.internal>
2024-07-14 06:51:52 +0000

    checked test, lint, format

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,9 +1,21 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from datetime import datetime, timedelta
-from moto import mock_aws as mock_dynamodb
+from unittest.mock import MagicMock, patch
 import boto3
+from moto import mock_dynamodb
+from datetime import datetime, timedelta
+import time
 from rss_aws_whatsnew.src.dynamodb import FeedEntryDB
+
+
+def wait_for_table(table):
+    while True:
+        try:
+            table.describe_table()
+            print("Table exists and is ready")
+            break
+        except table.meta.client.exceptions.ResourceNotFoundException:
+            print("Table is not ready yet, waiting...")
+            time.sleep(5)
 
 
 # テストケースの定義
@@ -14,6 +26,16 @@ class TestFeedEntryDB(unittest.TestCase):
         self.mock_dynamodb = mock_dynamodb()
         self.mock_dynamodb.start()
         self.dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
+        self.old_day = 7
+        # get now
+        self.now = datetime.now()
+        self.struct_now = time.localtime(time.mktime(self.now.timetuple()))
+
+        # get self.old_day+1 day
+        self.n_days_ago = self.now - timedelta(days=self.old_day + 1)
+        self.struct_n_days_ago = time.localtime(
+            time.mktime(self.n_days_ago.timetuple())
+        )
 
         # fmt: off
         # テスト用のテーブル作成
@@ -38,9 +60,7 @@ class TestFeedEntryDB(unittest.TestCase):
             },
         )
         # fmt: on
-        self.table.meta.client.get_waiter("table_exists").wait(
-            TableName=self.table_name
-        )
+        wait_for_table(self.table)  # テーブルが利用可能になるまで待機
 
         # FeedEntryDBクラスのインスタンス作成
         self.feed_entry_db = FeedEntryDB(self.table_name)
@@ -64,6 +84,7 @@ class TestFeedEntryDB(unittest.TestCase):
         self.assertEqual(table.name, new_table_name)
 
     def test_save_entries(self):
+        print("start test test_save_entries")
         # モックのRSSフィードエントリ
         mock_feed = MagicMock()
         # fmt: off
@@ -71,12 +92,14 @@ class TestFeedEntryDB(unittest.TestCase):
             {
                 "id": "1",
                 "title": "Test Entry 1",
-                "published_parsed": (2023, 5, 1, 10, 0, 0, 0, 0, 0),
+                "published": 'Mon, 01 Jan 2023 00:00:00 +0000',
+                'published_parsed': self.struct_now
             },
             {
                 "id": "2",
                 "title": "Test Entry 2",
-                "published_parsed": (2023, 5, 2, 12, 0, 0, 0, 0, 0),
+                "published": 'Jun, 01 Jan 2024 00:00:00 +0000',
+                'published_parsed': self.struct_n_days_ago
             },
         ]
         # fmt: on
@@ -86,36 +109,39 @@ class TestFeedEntryDB(unittest.TestCase):
 
         # テーブルからデータを取得
         response = self.table.scan()
+        print("res", response)
         items = response["Items"]
 
         # エントリが正しく保存されていることを確認
         self.assertEqual(len(items), 2)
         self.assertEqual(items[0]["id"], "1")
         self.assertEqual(items[0]["title"], "Test Entry 1")
-        self.assertEqual(items[0]["published_parsed"], "2023-05-01 10:00:00")
+        self.assertEqual(
+            items[0]["published_parsed"].strftime("%Y-%m-%d %H:%M:%S"),
+            "2023-01-01 00:00:00",
+        )
         self.assertEqual(items[1]["id"], "2")
         self.assertEqual(items[1]["title"], "Test Entry 2")
-        self.assertEqual(items[1]["published_parsed"], "2023-05-02 12:00:00")
+        self.assertEqual(
+            items[1]["published_parsed"].strftime("%Y-%m-%d %H:%M:%S"),
+            "2024-01-01 00:00:00",
+        )
 
     def test_get_recent_entries(self):
+        print("start test test_get_recent_entries")
         # テストデータの準備
-        now = datetime.now()
         self.table.put_item(
             Item={
                 "id": "1",
-                "title": "Old Entry",
-                "published_parsed": (now - timedelta(days=8)).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                ),
+                "title": "Recent Entry",
+                "published_parsed": self.struct_now,
             }
         )
         self.table.put_item(
             Item={
                 "id": "2",
-                "title": "Recent Entry",
-                "published_parsed": (now - timedelta(days=3)).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                ),
+                "title": "Old Entry",
+                "published_parsed": self.struct_n_days_ago,
             }
         )
 
@@ -124,39 +150,37 @@ class TestFeedEntryDB(unittest.TestCase):
 
         # 結果の検証
         self.assertEqual(len(recent_entries), 1)
-        self.assertEqual(recent_entries[0]["id"], "2")
+        self.assertEqual(recent_entries[0]["id"], "1")
         self.assertEqual(recent_entries[0]["title"], "Recent Entry")
 
     def test_delete_old_entries(self):
-        # テストデータの準備
-        now = datetime.now()
         self.table.put_item(
             Item={
                 "id": "1",
-                "title": "Old Entry",
-                "published_parsed": (now - timedelta(days=40)).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                ),
-            }
+                "title": "Recent Entry",
+                "summary": "Recent Summary",
+                "published": "Mon, 01 Jan 2023 00:00:00 +0000",
+                "published_parsed": self.struct_now,
+            },
         )
         self.table.put_item(
             Item={
                 "id": "2",
-                "title": "Recent Entry",
-                "published_parsed": (now - timedelta(days=10)).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                ),
+                "title": "Old Entry",
+                "summary": "Old Summary",
+                "published": "Mon, 01 Jan 2022 00:00:00 +0000",
+                "published_parsed": self.struct_n_days_ago,
             }
         )
 
-        # 30日以上前のエントリを削除
-        self.feed_entry_db.delete_old_entries(days=30)
+        # 7日以上前のエントリを削除
+        self.feed_entry_db.delete_old_entries(days=7)
 
         # 結果の検証
         response = self.table.scan()
         items = response["Items"]
         self.assertEqual(len(items), 1)
-        self.assertEqual(items[0]["id"], "2")
+        self.assertEqual(items[0]["id"], "1")
         self.assertEqual(items[0]["title"], "Recent Entry")
 
     def test_contains_service_word(self):
